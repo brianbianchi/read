d474522a2df10dbd8449ac283adcba0e869c1aa1
brianbianchi <bianchibrianj@gmail.com>
2024-05-15 23:02:42 -0400

    add domain

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') or 'TESTSECRETKEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == "True"
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', '8r1.dev']
 
 
 # Application definition
